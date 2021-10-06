efcd58ad47c7af6210839cd4c1c364bfe83710ad
Andrew Martinez <andrewmartinez6313@gmail.com>
2021-10-06 10:10:34 -0500

    starting employe.test.js

---
+++ test/Employee.test.js
@@ -0,0 +1,50 @@
+const Employee = require('../lib/Employee');
+
+describe('Employee tess', () => {
+    /*
+    - test 1
+    - should instantiate the Employee instance
+    */
+
+
+    /*
+    - test 2
+    - should set name of employee using constructor arguments
+    */
+
+
+    /*
+    - test 3
+    - should set the ID of the employee using constructor arguments
+    */
+
+
+     /*
+    - test 4
+    - should set email of employee using constructor arguments
+    */
+
+
+     /*
+    - test 5
+    - should get name using getName()
+    */
+
+
+     /*
+    - test 6
+    - should get ID using getID()
+    */
+
+
+     /*
+    - test 7
+    - should get email using getEmail()
+    */
+
+
+     /*
+    - test 8
+    - should get role using getRol() and return type of Employee
+    */
+})
