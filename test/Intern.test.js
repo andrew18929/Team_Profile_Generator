b701606b60b009280bf418638004e5a5686b39b3
Andrew Martinez <andrewmartinez6313@gmail.com>
2021-10-06 11:36:50 -0500

    started Intern.test.js

---
+++ test/Intern.test.js
@@ -0,0 +1,19 @@
+const Intern = require('../lib/Intern');
+
+// start of Intern Test
+describe('Inter test', () => {
+    /*
+    - test 1
+    - should set school name using constructor arguments
+    */
+
+    /*
+    - test 2
+    - should use getRole() and return it as Intern
+    */
+
+    /*
+    - test 3
+    - should get school name using getSchool()
+    */
+}
