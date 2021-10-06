21f72bcc57606c50e2417c50eade8b15cafa6e4f
Andrew Martinez <andrewmartinez6313@gmail.com>
2021-10-06 11:58:07 -0500

    coded Manager.test.js

---
+++ test/Manager.test.js
@@ -0,0 +1,38 @@
+const Manager = require('../lib/Manager');
+
+// start of Manager Test
+describe('Manager test', () => {
+    /*
+    - test 1
+    - should set office number using constructor arguments
+    */
+   it('should set office number using constructor arguments', () => {
+       const testOffNum = 210;
+       const eo = new Manager('John', 101, 'test@email.com', testOffNum);
+       expect(eo.officeNumber).toBe(testOffNum);
+   });
+
+    /*
+    - test 2
+    - should use getRole() and return it as Manager
+    */
+   describe('getRole', () => {
+       it('should use getRole() and return it as Manager', () => {
+           const testRole = 'Manager';
+           const eo = new Manager('John', 101, 'test@email.com', 210);
+           expect(eo.getRole()).toBe(testRole);
+       });
+   });
+
+    /*
+    - test 3
+    - should get office number by using getOfficeNumber()
+    */
+   describe('getOfficeNumber', () => {
+       it('should get office number by using getOfficeNumber()', () => {
+           const testOffNum = 210;
+           const eo = new Manager('John', 101, 'test@email.com', testOffNum);
+           expect(eo.getOfficeNumber()).toBe(testOffNum);
+       });
+   });
+});
