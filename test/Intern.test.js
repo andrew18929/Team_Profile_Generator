58580dd86701ff620476435c88fa5d6d40793e0a
Andrew Martinez <andrewmartinez6313@gmail.com>
2021-10-06 11:44:36 -0500

    coded Intern.test.js

--- test/Intern.test.js
+++ test/Intern.test.js
@@ -1,19 +1,38 @@
 const Intern = require('../lib/Intern');
 
 // start of Intern Test
-describe('Inter test', () => {
+describe('Intern test', () => {
     /*
     - test 1
     - should set school name using constructor arguments
     */
+   it('should set school name using constructor arguments', () => {
+       const testSchool = 'UTSA';
+       const eo = new Intern('John', 101, 'test@email.com', testSchool);
+       expect(eo.school).toBe(testSchool);
+   });
 
     /*
     - test 2
     - should use getRole() and return it as Intern
     */
+   describe('getRole', () => {
+       it('should use getRole() and return it as Intern', () => {
+           const testRole = 'Intern';
+           const eo = new Intern('John', 101, 'test@email.com', 'UTSA');
+           expect(eo.getRole()).toBe(testRole);
+       });
+   });
 
     /*
     - test 3
     - should get school name using getSchool()
     */
-}
+   describe('getSchool', () => {
+       it('should get school name using getSchool()', () => {
+           const testSchool = 'UTSA'
+           const eo = new Intern('John', 101, 'test@email.com', testSchool);
+           expect(eo.getSchool()).toBe(testSchool);
+       });
+   });
+});
