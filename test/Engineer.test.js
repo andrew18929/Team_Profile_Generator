129025b5620440d87b383b6c435b13da77291956
Andrew Martinez <andrewmartinez6313@gmail.com>
2021-10-06 11:33:04 -0500

    coded Engineer.test.js

--- test/Engineer.test.js
+++ test/Engineer.test.js
@@ -6,14 +6,33 @@ describe('Engineer test', () => {
     - test 1
     - should set GitHub username account using constructor arguments
     */
+   it('should set GitHub username account using constructor arguments', () => {
+       const testUserName = "GitHubUser";
+       const eo = new Engineer('John', 101, 'test@email.com', testUserName);
+       expect(eo.github).toBe(testUserName);
+   });
 
     /*
     - test 2
     - should use getRole() and return it as Engineer
     */
+   describe('getRole', () => {
+       it('should use getRole() and return it as Engineer', () => {
+           const testRole = 'Engineer';
+           const eo = new Engineer('John', 101, 'test@email.com', 'GitHubUser');
+           expect(eo.getRole()).toBe(testRole);
+       });
+   });
 
     /*
     - test 3
     - should get GitHub username using getGitHub()
     */
-})
+   describe('getGitHub', () => {
+       it('should get GitHub username using getGitHub()', () => {
+           const testUserName = 'GitHubUser';
+           const eo = new Engineer('John', 101, 'test@email.com', testUserName);
+           expect(eo.getGithub()).toBe(testUserName);
+       });
+   });
+});
