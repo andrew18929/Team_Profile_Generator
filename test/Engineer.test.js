81ace2a466e241d7653af5f3e0cd395c5e2d3bc6
Andrew Martinez <andrewmartinez6313@gmail.com>
2021-10-06 11:19:27 -0500

    started Engineer.test.js

---
+++ test/Engineer.test.js
@@ -0,0 +1,19 @@
+const Engineer = require('../lib/Engineer');
+
+// start of Engineer Test
+describe('Engineer test', () => {
+    /*
+    - test 1
+    - should set GitHub username account using constructor arguments
+    */
+
+    /*
+    - test 2
+    - should use getRole() and return it as Engineer
+    */
+
+    /*
+    - test 3
+    - should get GitHub username using getGitHub()
+    */
+})
